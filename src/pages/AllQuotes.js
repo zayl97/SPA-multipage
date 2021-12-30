7edc71285a15d7dbcb2302eff0f5b74127e2d09e
Kamil <kamil.plociennik@edu.wsti.pl>
2021-12-30 18:11:30 +0100

    Added basic layout elements
    Added main nav and quote items and forms

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is fun!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -3,10 +3,19 @@ import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is fun!" },
+];
+
 const QuoteDetails = () => {
   const params = useParams();
 
-    console.log(`/quotes/${params.quoteId}/comments`);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <section>
